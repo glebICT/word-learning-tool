0d2c194624f8fd290aa0babd707eb8f48ea43521
Gleb Svechnikov <g.svechnikov@infoculture.ru>
2024-02-25 20:33:43 +0400

    Add Word Learning Game

---
+++ script.js
@@ -0,0 +1,19 @@
+const words = [ "banana", "cat", "elephant", "door", "chair", "giraffe"];
+const randomIndex = Math.floor(Math.random() * words.length);
+const targetWord = words[randomIndex];
+window.onload = function () {
+
+    const wordElement = document.getElementById("word");
+    wordElement.textContent = "_ ".repeat(targetWord.length);
+    console.log(targetWord);
+
+};
+function checkGuess() {
+    const guess = document.getElementById("input").value.toLowerCase();
+    if (guess === targetWord) {
+        alert("Correct!");
+        // location.reload(); 
+    } else {
+        alert("Try again!");
+    }
+}
